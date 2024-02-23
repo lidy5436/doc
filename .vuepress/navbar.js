efa50e53e2803a6210983c86edae28bdc97fb39b
lidy <731729142@qq.com>
2024-02-23 20:59:03 +0800

    【新增】菜单自动加载

--- .vuepress/navbar.js
+++ .vuepress/navbar.js
@@ -1,25 +1,16 @@
-module.exports = [
+const modules = require('../modules')
+let navbar = [
     {
-      text: '首页',
-      link: '/'
-    },
-    {
-        text: '后端技术',
-        items: [
-            {text:'技巧-开发中使用的一些小技巧',link: '/后端技术/技巧-开发中使用的一些小技巧.md'},
-            {text:'防火墙配置-在centos中配置防火墙',link: '/后端技术/防火墙配置-在centos中配置防火墙.md'},
-        ]
-    },
-    {
-        text: '前端技术',
-        items: [
-            {text:'前端防抖-通过自定义指令实现',link: '/前端技术/前端防抖-通过自定义指令实现.md'},
-            {text:'案例-代码流星雨',link: '/前端技术/案例-代码流星雨.md'},
-            {text:'技巧-前端小技巧',link: '/前端技术/技巧-前端小技巧.md'},
-        ]
-    },
+        text: '首页',
+        link: '/'
+    }
+]
+navbar = navbar.concat(modules)
+navbar.push(
     {
         text: '常用书签',
         link: 'http://120.48.99.149:1888/'
-    },
-]
+    }
+)
+
+module.exports = navbar
--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -1,12 +1,9 @@
-module.exports ={
-    '/后端技术/': [
-        '防火墙配置-在centos中配置防火墙',
-        '技巧-开发中使用的一些小技巧'
-    ],
-    '/前端技术/': [
-        '前端防抖-通过自定义指令实现',
-        '案例-代码流星雨',
-        '技巧-前端小技巧'
-    ],
-    "/": "auto",
-}
+const modules = require('../modules')
+
+let sidebar = {}
+
+modules.forEach(item => {
+    sidebar['/'+item.text+'/'] = item.items.map(o => o.text)
+})
+
+module.exports = sidebar;
