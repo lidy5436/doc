d3bfcf56b63f8acb39e8792cf7afa826fd38cb91
lidy <731729142@qq.com>
2024-02-23 23:04:59 +0800

    【新增】添加新菜单

--- modules.js
+++ modules.js
@@ -5,6 +5,7 @@ module.exports = [
             {text:'技巧-开发中使用的一些小技巧',link: '/后端技术/技巧-开发中使用的一些小技巧.md'},
             {text:'笔记-Linux学习笔记',link: '/后端技术/笔记-Linux学习笔记.md'},
             {text:'防火墙配置-在centos中配置防火墙',link: '/后端技术/防火墙配置-在centos中配置防火墙.md'},
+            {text:'SpringBoot项目启动控制台输出访问地址',link: '/后端技术/SpringBoot项目启动控制台输出访问地址.md'},
         ]
     },
     {
